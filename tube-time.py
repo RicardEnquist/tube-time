306ce84e42adca3efd38f262ce0af87b1cd92b50
Ricard <tillricard@gmail.com>
2022-02-03 16:44:48 +0100

    Added refreshing of API data and made line color field dynamic

--- tube-time.py
+++ tube-time.py
@@ -6,11 +6,6 @@ import ssl
 import datetime
 from tkinter import *
 
-# Ignore SSL certificate errors
-ctx = ssl.create_default_context()
-ctx.check_hostname = False
-ctx.verify_mode = ssl.CERT_NONE
-
 # Define colors
 bgcolor = "#4F5257"
 dispBgColor = "#110F04"
@@ -18,13 +13,26 @@ dispTextColor = "#DED311"
 lineColorGreen = "#00A748"
 lineColorBlue = "#0095D2"
 lineColorRed = "#E40315"
+lineColors = {
+    "10": lineColorBlue,
+    "11": lineColorBlue,
+    "13": lineColorRed,
+    "14": lineColorRed,
+    "17": lineColorGreen,
+    "18": lineColorGreen,
+    "19": lineColorGreen,
+    }
+
+
+
+# Configuration
+refInt = 20000
+stationKey = "9ef9290d-c9ae-409c-9862-2b0e5d847b2a"
+departureKey = "72d054f6-6680-4584-b7ed-5097956524aa"
+station = "Akalla"
 
 # TEST VAVLUES TO BE AUTOMATED
 lineColor = lineColorGreen
-departureDirection1 = "Akalla"
-departureTime1 = "10 min"
-departureDirection2 = "Kungsträdgården"
-departureTime2 = "13 min"
 
 root = Tk()
 root.title("Tube-Time")
@@ -38,16 +46,88 @@ root.configure(background=bgcolor)
 # Add icon
 root.iconbitmap('icon.ico')
 
+
+def getDepartures():
+    # Ignore SSL certificate errors
+    ctx = ssl.create_default_context()
+    ctx.check_hostname = False
+    ctx.verify_mode = ssl.CERT_NONE
+
+    stationurl = "https://api.resrobot.se/v2/location.name?"
+    paramsStation = dict(
+    input = station,
+    format = "json",
+    key = stationKey
+    )
+
+    url = stationurl + urllib.parse.urlencode(paramsStation)
+    uh = urllib.request.urlopen(url, context=ctx)
+    data = uh.read().decode()
+    js = json.loads(data)
+    stationID = js["StopLocation"][0]["id"]
+    print(stationID)
+
+    departureurl = "https://api.resrobot.se/v2/departureBoard?"
+    paramsDeparture = dict(
+    key = departureKey,
+    id = stationID,
+    maxJourneys = 6,
+    products = 32,
+    passlist = 0,
+    format = "json"
+    )
+
+    url = departureurl + urllib.parse.urlencode(paramsDeparture)
+    uh = urllib.request.urlopen(url, context=ctx)
+    data = uh.read().decode()
+    js = json.loads(data)
+    print(json.dumps(js, indent=4))
+
+    global departureDirection1
+    global departureDirection2
+    global departureDirection3
+    departureDirection1 = re.findall("^\S+", js["Departure"][0]["direction"])[0]
+    departureDirection2= re.findall("^\S+", js["Departure"][1]["direction"])[0]
+    departureDirection3 = re.findall("^\S+", js["Departure"][2]["direction"])[0]
+  
+    #"""Loop all of this shit, for every departure where the name is the same (single direction)"""
+    arrival1_time = datetime.datetime.combine(datetime.date.today(), datetime.datetime.strptime(js["Departure"][0]["time"], "%H:%M:%S").time())
+    arrival2_time = datetime.datetime.combine(datetime.date.today(), datetime.datetime.strptime(js["Departure"][1]["time"], "%H:%M:%S").time())
+    arrival3_time = datetime.datetime.combine(datetime.date.today(), datetime.datetime.strptime(js["Departure"][2]["time"], "%H:%M:%S").time())
+
+    current_time = datetime.datetime.now()
+    waiting1_time = (arrival1_time - current_time)
+    waiting2_time = (arrival2_time - current_time)
+    waiting3_time = (arrival3_time - current_time)
+
+    global departureTime1
+    global departureTime2
+    global departureTime3
+
+    departureTime1 = str(int(waiting1_time.total_seconds() / 60.0)) + " " + "min"
+    departureTime2 = str(int(waiting2_time.total_seconds() / 60.0)) + " " + "min"
+    departureTime3 = str(int(waiting3_time.total_seconds() / 60.0)) + " " + "min"
+    if (waiting1_time.total_seconds()/60) < 0: departureTime1 = "Nu"
+  
+    # Update tkinter labels with fresh departure data, re-run at refresh interval
+    primDispLabelStation.configure(text=departureDirection1)
+    primDispLabelTime.configure(text=departureTime1)
+    secDispLabel.configure(text=departureDirection2 + " " + departureTime2)
+    lineColorLabel.configure(bg=lineColors.get(js["Departure"][0]["Product"]["num"]))
+    root.after(refInt, getDepartures)
+    
+
 # Create line color label
 relWidth = 0.94
 relHeight = 0.2
-lineColorLabel = Label(root, bg=lineColor)
+lineColorLabel = Label(root, bg=lineColorGreen)
 lineColorLabel.place(
     relwidth=relWidth,
     relheight=relHeight,
     relx=((1-relWidth)/2),
     rely=1-relHeight)
 
+# Create frame for departure information labels
 dispFrame = Frame(root, bg=dispBgColor)
 dispFrame.place(
     relwidth=relWidth,
@@ -67,7 +147,7 @@ primDispLabelStation = Label(
     font=("Helvetica 40 bold"),
     padx=10,
     pady=0,
-    text=departureDirection1)
+    text="")
 
 primDispLabelStation.grid(
     row = 0,
@@ -82,7 +162,7 @@ primDispLabelTime = Label(
     font=("Helvetica 40 bold"),
     padx=10,
     pady=0,
-    text=departureTime1)
+    text="")
 
 primDispLabelTime.grid(
     row=0,
@@ -97,7 +177,7 @@ secDispLabel = Label(
     font=("Helvetica 40 bold"),
     padx=10,
     pady=0,
-    text=departureDirection2 + "  " + departureTime2)
+    text="")
 
 secDispLabel.grid(
     row=1,
@@ -105,61 +185,7 @@ secDispLabel.grid(
     columnspan=2,
     sticky="sw")
 
-def getDepartures():
-    global station
-    station = stationInput.get()
-    #stationLabel = Label(root, text=stationInput.get())
-    #stationLabel.grid(row=4, column=0)
-
-    stationurl = "https://api.resrobot.se/v2/location.name?"
-    paramsStation = dict(
-    input = station,
-    format = "json",
-    key = "9ef9290d-c9ae-409c-9862-2b0e5d847b2a"
-    )
-
-    url = stationurl + urllib.parse.urlencode(paramsStation)
-    uh = urllib.request.urlopen(url, context=ctx)
-    data = uh.read().decode()
-    js = json.loads(data)
-    stationID = js["StopLocation"][0]["id"]
-    print(stationID)
-
-    departureurl = "https://api.resrobot.se/v2/departureBoard?"
-    paramsDeparture = dict(
-    key = "72d054f6-6680-4584-b7ed-5097956524aa",
-    id = stationID,
-    maxJourneys = 6,
-    products = 32,
-    passlist = 0,
-    format = "json"
-    )
-
-    url = departureurl + urllib.parse.urlencode(paramsDeparture)
-    uh = urllib.request.urlopen(url, context=ctx)
-    data = uh.read().decode()
-    js = json.loads(data)
-    print(json.dumps(js, indent=4))
-
-    arrival1_time = datetime.datetime.combine(datetime.date.today(), datetime.datetime.strptime(js["Departure"][0]["time"], "%H:%M:%S").time())
-    arrival2_time = datetime.datetime.combine(datetime.date.today(), datetime.datetime.strptime(js["Departure"][1]["time"], "%H:%M:%S").time())
-    arrival3_time = datetime.datetime.combine(datetime.date.today(), datetime.datetime.strptime(js["Departure"][2]["time"], "%H:%M:%S").time())
-
-    current_time = datetime.datetime.now()
-    waiting1_time = (arrival1_time - current_time)
-    waiting2_time = (arrival2_time - current_time)
-    waiting3_time = (arrival3_time - current_time)
-    waiting1_min = waiting1_time.total_seconds() / 60.0
-    waiting2_min = waiting2_time.total_seconds() / 60.0
-    waiting3_min = waiting3_time.total_seconds() / 60.0
-
-    if waiting1_min < 0:
-        departure1 = re.findall("^\S+", js["Departure"][0]["direction"])[0] + " " + "Nu"
-        departure2 = re.findall("^\S+", js["Departure"][1]["direction"])[0] + " " + str(int(waiting2_min)) + " min"
-        departure3 = re.findall("^\S+", js["Departure"][1]["direction"])[0] + " " + str(int(waiting3_min)) + " min"
-    else:
-        departure1 = re.findall("^\S+", js["Departure"][0]["direction"])[0] + " " + str(int(waiting1_min)) + " min"
-        departure2 = re.findall("^\S+", js["Departure"][1]["direction"])[0] + " " + str(int(waiting2_min)) + " min"
-        departure3 = re.findall("^\S+", js["Departure"][1]["direction"])[0] + " " + str(int(waiting3_min)) + " min"
+# Bring up empty main window before calling API first time
+root.after(10, getDepartures)
 
 root.mainloop()
